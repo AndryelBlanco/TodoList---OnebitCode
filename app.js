25859e5417744dd572dbe732dc77c1a2a540ca26
Andryel <andryelmblanco@gmail.com>
2022-07-05 21:24:49 -0300

    Adicionando rota post

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
 const express = require('express');
+const checkListRouter = require('./src/routes/checklist');
+
 
 const app = express();
-
 app.use(express.json());
+app.use('/checklist', checkListRouter);
 
-//Criando um middleware
-const log = (req, res, next) => {
-  console.log(req.body);
-  console.log('DATE', Date.now());
-  next();
-}
 
-app.use(log);
-
-//Para criar a rota
-app.get('/', (req, res) => {
-  res.send('<h1>My todo list ;)</h1>');
-});
-
-app.get('/json', (req, res) => {
-  // Devolver um JSON
-  res.json({
-    title: 'Tarefa Inicial',
-    status: 'Done'
-  }) 
-});
-
-//Fazemos o server ouvir a porta 3000
-app.listen(3000, () => {
-  console.log("SERVIDOR FOI INICIADO");
-});
+app.listen(3000, ()=>{
+  console.log('Server iniciado');
+})
